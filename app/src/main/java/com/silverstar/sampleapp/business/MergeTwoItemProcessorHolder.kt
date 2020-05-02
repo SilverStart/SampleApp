6da7957dd1225f1caaa797d0796ba84ca4567355
esahn <esahn@huray.net>
2020-05-02 19:24:35 +0900

    MergeTwoItemProcessorHolder 클래스 구현 완료

--- app/src/main/java/com/silverstar/sampleapp/business/MergeTwoItemProcessorHolder.kt
+++ app/src/main/java/com/silverstar/sampleapp/business/MergeTwoItemProcessorHolder.kt
@@ -1,13 +1,47 @@
 package com.silverstar.sampleapp.business
 
 import com.silverstar.sampleapp.business.base.ProcessorHolder
+import com.silverstar.sampleapp.data.dao.ItemDao
 import com.silverstar.sampleapp.data.entity.Item
 import com.silverstar.sampleapp.data.pojo.ItemFromServer
+import com.silverstar.sampleapp.utils.Error
+import com.silverstar.sampleapp.utils.Result
+import com.silverstar.sampleapp.utils.toResult
 import io.reactivex.ObservableTransformer
+import io.reactivex.rxkotlin.Observables
 import javax.inject.Inject
 
-class MergeTwoItemProcessorHolder @Inject constructor() :
-    ProcessorHolder<Pair<List<ItemFromServer>, List<Item>>, List<Item>> {
-    override val processor: ObservableTransformer<Pair<List<ItemFromServer>, List<Item>>, List<Item>>
-        get() = TODO("Not yet implemented")
+class MergeTwoItemProcessorHolder @Inject constructor(
+    loadItemByPageProcessorHolder: ProcessorHolder<Int, Result<List<ItemFromServer>>>,
+    itemDao: ItemDao
+) :
+    ProcessorHolder<Int, Result<List<Item>>> {
+
+    override val processor = ObservableTransformer<Int, Result<List<Item>>> { observable ->
+        Observables.combineLatest(
+            observable.compose(loadItemByPageProcessorHolder.processor),
+            itemDao.getAll()
+        ) { a, b ->
+            if (a is Result.OnError) error(a.error)
+            else compare((a as Result.OnSuccess<List<ItemFromServer>>).data, b)
+        }
+    }
+
+    private fun error(error: Error): Result<List<Item>> = error.toResult()
+
+    private fun compare(targetList: List<ItemFromServer>, comparedList: List<Item>)
+            : Result<List<Item>> {
+        return targetList.map {
+            val liked = comparedList.find { compared -> it.isEquals(compared) } != null
+            Item(it.name, it.thumbnailUrl, it.description.imageUrl, it.rate, liked)
+        }.toResult()
+    }
+
+    private fun ItemFromServer.isEquals(compared: Item): Boolean {
+        return name == compared.name &&
+                thumbnailUrl == compared.thumbnailUrl &&
+                description.imageUrl == compared.imageUrl &&
+                rate == compared.rate
+    }
+
 }
---
+++ app/src/test/java/com/silverstar/sampleapp/business/MergeTwoItemProcessorHolderTest.kt
@@ -0,0 +1,225 @@
+package com.silverstar.sampleapp.business
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.silverstar.sampleapp.business.base.ProcessorHolder
+import com.silverstar.sampleapp.data.dao.ItemDao
+import com.silverstar.sampleapp.data.entity.Item
+import com.silverstar.sampleapp.data.pojo.Description
+import com.silverstar.sampleapp.data.pojo.ItemFromServer
+import com.silverstar.sampleapp.utils.Result
+import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("MergeTwoItemProcessorHolder 클래스")
+class MergeTwoItemProcessorHolderTest {
+
+    private val loadItemByPageProcessorHolder: ProcessorHolder<Int, Result<List<ItemFromServer>>> =
+        mock()
+
+    private val itemDao: ItemDao = mock()
+
+    private lateinit var mergeTwoItemProcessorHolder: MergeTwoItemProcessorHolder
+
+    private fun getEmptyItemFromServerList(): List<ItemFromServer> = emptyList()
+
+    private fun getEmptyItemList(): List<Item> = emptyList()
+
+    private fun getItemListThatHasNItems(n: Int): List<Item> {
+        val listThatHasNItems = ArrayList<Item>()
+
+        repeat(n) {
+            listThatHasNItems.add(
+                Item("name", "thumbnailUrl", "imageUrl", 10f, false)
+            )
+        }
+
+        return listThatHasNItems
+    }
+
+    @Nested
+    @DisplayName("빈 List 를 리턴하는 LoadItemByPageProcessorHolder 와")
+    inner class ContextWithLoadItemByPageProcessorHolderThatReturnsEmptyList {
+
+        private val emptyItemFromServerList: List<ItemFromServer> = getEmptyItemFromServerList()
+
+        @BeforeEach
+        fun prepareMock() {
+            whenever(loadItemByPageProcessorHolder.processor)
+                .thenReturn(
+                    ObservableTransformer {
+                        it.flatMap { Observable.just(Result.OnSuccess(emptyItemFromServerList)) }
+                    }
+                )
+        }
+
+        @Nested
+        @DisplayName("빈 List 를 리턴하는 ItemDao 를 주입 시")
+        inner class ContextWithItemDaoThatReturnsEmptyList {
+
+            private val emptyItemList: List<Item> = getEmptyItemList()
+
+            @BeforeEach
+            fun prepareMock() {
+                whenever(itemDao.getAll())
+                    .thenReturn(Observable.just(emptyItemList))
+            }
+
+            @Test
+            @DisplayName("데이터가 흘러올 경우 빈 List 를 리턴한다")
+            fun itReturnsEmptyList() {
+                mergeTwoItemProcessorHolder = MergeTwoItemProcessorHolder(
+                    loadItemByPageProcessorHolder,
+                    itemDao
+                )
+
+                Observable.just(1)
+                    .compose(mergeTwoItemProcessorHolder.processor)
+                    .test()
+                    .assertValue(Result.OnSuccess(emptyItemList))
+            }
+        }
+
+        @Nested
+        @DisplayName("1개의 아이템을 가진 List 를 리턴하는 ItemDao 를 주입 시")
+        inner class ContextWithItemDaoThatReturnsListThatHas1Item {
+
+            private val emptyItemList: List<Item> = getEmptyItemList()
+
+            private val listThatHas1Item: List<Item> = getItemListThatHasNItems(1)
+
+            @BeforeEach
+            fun prepareMock() {
+                whenever(itemDao.getAll())
+                    .thenReturn(Observable.just(listThatHas1Item))
+            }
+
+            @Test
+            @DisplayName("데이터가 흘러올 경우 빈 List 를 리턴한다")
+            fun itReturnsEmptyList() {
+                mergeTwoItemProcessorHolder = MergeTwoItemProcessorHolder(
+                    loadItemByPageProcessorHolder,
+                    itemDao
+                )
+
+                Observable.just(1)
+                    .compose(mergeTwoItemProcessorHolder.processor)
+                    .test()
+                    .assertValue(Result.OnSuccess(emptyItemList))
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("3개의 아이템을 가진 List 를 리턴하는 LoadItemByPageProcessorHolder 와")
+    inner class ContextWithLoadItemByPageProcessorHolderThatReturnsListThatHas3Items {
+
+        private val itemFromServerListThatHas3Items: List<ItemFromServer> =
+            listOf(
+                ItemFromServer(
+                    "name",
+                    "thumbnailUrl",
+                    Description("imageUrl"),
+                    10f
+                ),
+                ItemFromServer(
+                    "name2",
+                    "thumbnailUrl",
+                    Description("imageUrl"),
+                    8f
+                ),
+
+                ItemFromServer(
+                    "name3",
+                    "thumbnailUrl",
+                    Description("imageUrl"),
+                    9f
+                )
+            )
+
+        @BeforeEach
+        fun prepareMock() {
+            whenever(loadItemByPageProcessorHolder.processor)
+                .thenReturn(
+                    ObservableTransformer {
+                        it.flatMap {
+                            Observable.just(Result.OnSuccess(itemFromServerListThatHas3Items))
+                        }
+                    }
+                )
+        }
+
+        @Nested
+        @DisplayName("빈 List 를 리턴하는 ItemDao 를 주입 시")
+        inner class ContextWithItemDaoThatReturnsEmptyList {
+            private val emptyItemList: List<Item> = getEmptyItemList()
+
+            private val itemListThatHas3Items: List<Item> = listOf(
+                Item("name", "thumbnailUrl", "imageUrl", 10f, false),
+                Item("name2", "thumbnailUrl", "imageUrl", 8f, false),
+                Item("name3", "thumbnailUrl", "imageUrl", 9f, false)
+            )
+
+            @BeforeEach
+            fun prepareMock() {
+                whenever(itemDao.getAll())
+                    .thenReturn(Observable.just(emptyItemList))
+            }
+
+            @Test
+            @DisplayName("데이터가 흘러들어올 경우 3개의 아이템을 가진 List 를 리턴한다")
+            fun itReturnsListThatHas3Items() {
+                mergeTwoItemProcessorHolder = MergeTwoItemProcessorHolder(
+                    loadItemByPageProcessorHolder,
+                    itemDao
+                )
+
+                Observable.just(1)
+                    .compose(mergeTwoItemProcessorHolder.processor)
+                    .test()
+                    .assertValue(Result.OnSuccess(itemListThatHas3Items))
+            }
+        }
+
+        @Nested
+        @DisplayName("1개의 아이템을 가진 List 를 리턴하는 ItemDao 를 주입 시")
+        inner class ContextWithItemDaoThatReturnsListThatHas1Item {
+
+            private val listThatHas1Item: List<Item> = listOf(
+                Item("name", "thumbnailUrl", "imageUrl", 10f, true)
+            )
+
+            private val itemListWhichIsCompared: List<Item> = listOf(
+                Item("name", "thumbnailUrl", "imageUrl", 10f, true),
+                Item("name2", "thumbnailUrl", "imageUrl", 8f, false),
+                Item("name3", "thumbnailUrl", "imageUrl", 9f, false)
+            )
+
+
+            @BeforeEach
+            fun prepareMock() {
+                whenever(itemDao.getAll())
+                    .thenReturn(Observable.just(listThatHas1Item))
+            }
+
+            @Test
+            @DisplayName("데이터가 흘러올 경우 ItemDao 에서 리턴한 List 와 비교 후 치환하여 리턴한다")
+            fun itReturnsListWhichIsComparedWithListReturnedFromItemDao() {
+                mergeTwoItemProcessorHolder = MergeTwoItemProcessorHolder(
+                    loadItemByPageProcessorHolder,
+                    itemDao
+                )
+
+                Observable.just(1)
+                    .compose(mergeTwoItemProcessorHolder.processor)
+                    .test()
+                    .assertValue(Result.OnSuccess(itemListWhichIsCompared))
+            }
+        }
+    }
+
+}
